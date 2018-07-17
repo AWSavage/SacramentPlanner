b4d91a61797b30e35ff6fde47cdfcea9b369b0d9
Andrew Savage <sav13005@byui.edu>
2018-07-17 08:56:28 -0600

    Started to add Student Class

---
+++ SacramentPlanner/Models/Speaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SacramentPlanner.Models
+{
+    public class Speaker
+    {
+            public int SpeakerID { get; set; }
+            public String SpeakerName { get; set; }
+            public String Topic { get; set; }
+            public String SpeakerOrder { get; set; }
+
+    }
+}
